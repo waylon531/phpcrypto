cb02a0efb8eb6458c1d4831a12d724c8f518a553
Waylon Cude <waylon531@gmail.com>
2013-12-14 15:20:53 -0800

    Added ability to use caps and numbers

--- bruteforce.php
+++ bruteforce.php
@@ -3,12 +3,29 @@ ini_set('max_execution_time', 300);
 $hash = $_POST['hash'];
 echo $hash;
 echo "<br>";
-$array = range ('a', 'z');
-$array[26] = 9001;
+$increments = 0;
+
+$lowercase = range ('a', 'z'); //Array of all lowercase
+$uppercase = range ('A', 'Z'); //Array of all caps
+$numbers = range (0, 9); //Array of digits
 $GLOBALS['increment'] = 0;
 $GLOBALS['verify'] = false;
 $GLOBALS['success'] = 0;
-while ($GLOBALS['increment'] < 26 and $GLOBALS['verify'] == false) {
+$array = array();
+if $_POST['lowercase'] = true {
+    $increments = $increments + 26;
+    array_merge($array, $lowercase);
+}
+if $_POST['uppercase'] = true {
+    $increments = $increments + 26;  
+    array_merge($array, $lowercase);
+}
+if $_POST['numbers'] = true {
+    $increments = $increments + 10;  
+    array_merge($array, $lowercase);
+}
+$incrementvalue = $increments - 1;
+while ($GLOBALS['increment'] < $increments and $GLOBALS['verify'] == false) {
     if (md5($array[$GLOBALS['increment']]) == $hash) {
         echo $array[$GLOBALS['increment']];
         $GLOBALS['verify'] = true;
@@ -17,7 +34,7 @@ while ($GLOBALS['increment'] < 26 and $GLOBALS['verify'] == false) {
 }
 $GLOBALS['increment'] = 0;
 $GLOBALS['increment2'] = 0;
-while ($GLOBALS['increment2'] < 26 and $GLOBALS['verify'] == false) {
+while ($GLOBALS['increment2'] < $increments and $GLOBALS['verify'] == false) {
     $words = $array[$GLOBALS['increment2']] . $array[$GLOBALS['increment']];
     if (md5($words) == $hash) {
         echo $words;
@@ -26,7 +43,7 @@ while ($GLOBALS['increment2'] < 26 and $GLOBALS['verify'] == false) {
     //echo $words . "<br>";
     //echo $GLOBALS['increment'];
     //echo $GLOBALS['increment2'];
-    if($GLOBALS['increment'] == 25) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
+    if($GLOBALS['increment'] == $incrementvalue) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
     ++$GLOBALS['increment'];
     //echo "1<br>";
 
@@ -34,7 +51,7 @@ while ($GLOBALS['increment2'] < 26 and $GLOBALS['verify'] == false) {
 $GLOBALS['increment'] = 0;
 $GLOBALS['increment2'] = 0;
 $GLOBALS['increment3'] = 0;
-while ($GLOBALS['increment3'] < 26 and $GLOBALS['verify'] == false) {
+while ($GLOBALS['increment3'] < $increments and $GLOBALS['verify'] == false) {
     $words = $array[$GLOBALS['increment3']] . $array[$GLOBALS['increment2']] . $array[$GLOBALS['increment']];
     if (md5($words) == $hash) {
         echo $words;
@@ -43,8 +60,8 @@ while ($GLOBALS['increment3'] < 26 and $GLOBALS['verify'] == false) {
     //echo $words . "<br>";
     //echo $GLOBALS['increment'];
     //echo $GLOBALS['increment2'];
-    if($GLOBALS['increment'] == 25) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
-    if($GLOBALS['increment2'] == 25) {++$GLOBALS['increment3']; $GLOBALS['increment2'] = 0;}
+    if($GLOBALS['increment'] == $incrementvalue) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
+    if($GLOBALS['increment2'] == $incrementvalue) {++$GLOBALS['increment3']; $GLOBALS['increment2'] = 0;}
     ++$GLOBALS['increment'];
     //echo "1<br>";
 
@@ -53,7 +70,7 @@ $GLOBALS['increment'] = 0;
 $GLOBALS['increment2'] = 0;
 $GLOBALS['increment3'] = 0;
 $GLOBALS['increment4'] = 0;
-while ($GLOBALS['increment4'] < 26 and $GLOBALS['verify'] == false) {
+while ($GLOBALS['increment4'] < $increments and $GLOBALS['verify'] == false) {
     $words = $array[$GLOBALS['increment4']] . $array[$GLOBALS['increment3']] . $array[$GLOBALS['increment2']] . $array[$GLOBALS['increment']];
     if (md5($words) == $hash) {
         echo $words;
@@ -62,9 +79,9 @@ while ($GLOBALS['increment4'] < 26 and $GLOBALS['verify'] == false) {
     //echo $words . "<br>";
     //echo $GLOBALS['increment'];
     //echo $GLOBALS['increment2'];
-    if($GLOBALS['increment'] == 25) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
-    if($GLOBALS['increment2'] == 25) {++$GLOBALS['increment3']; $GLOBALS['increment2'] = 0;}
-    if($GLOBALS['increment3'] == 25) {++$GLOBALS['increment4']; $GLOBALS['increment3'] = 0;}
+    if($GLOBALS['increment'] == $incrementvalue) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
+    if($GLOBALS['increment2'] == $incrementvalue) {++$GLOBALS['increment3']; $GLOBALS['increment2'] = 0;}
+    if($GLOBALS['increment3'] == $incrementvalue) {++$GLOBALS['increment4']; $GLOBALS['increment3'] = 0;}
     ++$GLOBALS['increment'];
     //echo "1<br>";
 
@@ -74,7 +91,7 @@ $GLOBALS['increment2'] = 0;
 $GLOBALS['increment3'] = 0;
 $GLOBALS['increment4'] = 0;
 $GLOBALS['increment5'] = 0;
-while ($GLOBALS['increment5'] < 26 and $GLOBALS['verify'] == false) {
+while ($GLOBALS['increment5'] < $increments and $GLOBALS['verify'] == false) {
     $words = $array[$GLOBALS['increment5']] . $array[$GLOBALS['increment4']] . $array[$GLOBALS['increment3']] . $array[$GLOBALS['increment2']] . $array[$GLOBALS['increment']];
     if (md5($words) == $hash) {
         echo $words;
@@ -83,10 +100,10 @@ while ($GLOBALS['increment5'] < 26 and $GLOBALS['verify'] == false) {
     //echo $words . "<br>";
     //echo $GLOBALS['increment'];
     //echo $GLOBALS['increment2'];
-    if($GLOBALS['increment'] == 25) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
-    if($GLOBALS['increment2'] == 25) {++$GLOBALS['increment3']; $GLOBALS['increment2'] = 0;}
-    if($GLOBALS['increment3'] == 25) {++$GLOBALS['increment4']; $GLOBALS['increment3'] = 0;}
-    if($GLOBALS['increment4'] == 25) {++$GLOBALS['increment5']; $GLOBALS['increment4'] = 0;}
+    if($GLOBALS['increment'] == $incrementvalue) {++$GLOBALS['increment2']; $GLOBALS['increment'] = -1;}
+    if($GLOBALS['increment2'] == $incrementvalue) {++$GLOBALS['increment3']; $GLOBALS['increment2'] = 0;}
+    if($GLOBALS['increment3'] == $incrementvalue) {++$GLOBALS['increment4']; $GLOBALS['increment3'] = 0;}
+    if($GLOBALS['increment4'] == $incrementvalue) {++$GLOBALS['increment5']; $GLOBALS['increment4'] = 0;}
     ++$GLOBALS['increment'];
     //echo "1<br>";
 
