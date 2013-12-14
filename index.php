8c17baa1e8b3e52e2c54950352a0451549139b18
Waylon Cude <waylon531@gmail.com>
2013-12-14 14:55:30 -0800

    More Options

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 echo '<form name="input" action="/bruteforce.php" method="post">
+<input type="checkbox" name="mthod" value="lowercase">Chech lowercase letters<br>
+<input type="checkbox" name="method" value="uppercase">Check uppercase letters<br>
+<input type="checkbox" name="method" value="number">Check numbers<br>
 <input type="text" name="hash">
 <input type="submit" value="SUBMIT"';
         echo ">
 </form>";
-echo '<p>Password 5 characters or less with only lowercase letters in unsalted md5 will be cracked</p>'
+echo '<p>Password 5 characters or less in unsalted md5 will be cracked</p>'
 ?>
