a1a4e4018dad0e496767b10b6a5df5c08b9cbbae
Waylon Cude <waylon531@gmail.com>
2013-12-14 15:02:34 -0800

    Fixed typos

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo '<form name="input" action="/bruteforce.php" method="post">
-<input type="checkbox" name="mthod" value="lowercase">Chech lowercase letters<br>
-<input type="checkbox" name="method" value="uppercase">Check uppercase letters<br>
-<input type="checkbox" name="method" value="number">Check numbers<br>
+<input type="checkbox" name="method[]" value="lowercase">Chech lowercase letters<br>
+<input type="checkbox" name="method[]" value="uppercase">Check uppercase letters<br>
+<input type="checkbox" name="method[]" value="number">Check numbers<br>
 <input type="text" name="hash">
 <input type="submit" value="SUBMIT"';
         echo ">
