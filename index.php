f35b2767f7034a4c8a48dc8d6e106cb635a89c92
Waylon Cude <waylon531@gmail.com>
2013-12-14 15:04:21 -0800

    Fixed checkboxes

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo '<form name="input" action="/bruteforce.php" method="post">
-<input type="checkbox" name="method[]" value="lowercase">Chech lowercase letters<br>
-<input type="checkbox" name="method[]" value="uppercase">Check uppercase letters<br>
-<input type="checkbox" name="method[]" value="number">Check numbers<br>
+<input type="checkbox" name="lowercase" value="true">Check lowercase letters<br>
+<input type="checkbox" name="uppercase" value="true">Check uppercase letters<br>
+<input type="checkbox" name="numbers" value="true">Check numbers<br>
 <input type="text" name="hash">
 <input type="submit" value="SUBMIT"';
         echo ">
